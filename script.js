5aacdd8acc4a4a3df6e10cfc47a66c0f204d9689
matt keigwin <mkeigwin@bates.edu>
2016-11-17 23:38:48 -0500

    updates

--- script.js
+++ script.js
@@ -1,3 +1,43 @@
 $(document).ready(function() {
 console.log('linked')
+
+
+  const changeColor = (e) => {
+    let color = $(e.target).data('color')
+    console.log(color)
+    $(e.target).css('color', color)
+  }
+
+  const randomColor = (e) => {
+    const colorsILike = [
+      'deepskyblue',
+      'cyan',
+      'turquoise',
+      'mediumturquoise',
+      'aqua',
+      'darkturquoise',
+      'skyblue',
+      'lightskyblue',
+      'steelblue',
+      'dodgerblue',
+      'cornflowerblue',
+      'royalblue',
+      'midnightblue',
+      'navy',
+    ]
+    let colorChoice = Math.floor(Math.random() * colorsILike.length)
+    $(e.target).css('color', colorsILike[colorChoice])
+  }
+
+  const originalColor = () => {
+    setTimeout(function() {
+      $('.letter').css('color', 'white')
+    },100)
+    $('.letter').css('color', 'crimson')
+  }
+
+  $('.letter').on('mouseover', changeColor)
+  $('.letter').on('mouseout', randomColor)
+  $('.letter').on('click', originalColor)
+
 });
